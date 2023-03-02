159f603f014efb4380295936276b9663145bcbcf
Somerandomidiot <90102713+Somerandomidiot@users.noreply.github.com>
2023-03-02 23:01:12 +0545

    Update FlyingSim_DiamondFarm.lua

--- FlyingSim_DiamondFarm.lua
+++ FlyingSim_DiamondFarm.lua
@@ -1,1 +1,13 @@
-later
+while task.wait(15) do
+
+for i= 1, 100 do
+  
+  game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Diamonds:GetChildren()[i].CFrame 
+  
+  task.wait(.1)
+  
+ end
+
+task.wait(3)
+
+game:GetService("TeleportService"):Teleport(game.PlaceId, game:GetService("Players").LocalPlayer)
